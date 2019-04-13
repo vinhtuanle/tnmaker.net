7915b46e8254dbc7fb8e540b1e8e8d53e073df3e
Tran Quang Ngoc <ngoctigerstyle@gmail.com>
2019-04-13 23:09:05 +0700

    #2 : Fix cung textare, on/off input, bo phieu 120, 100 bo luu y

--- wp-content/themes/twentynineteen/phieu-trac-nghiem.php
+++ wp-content/themes/twentynineteen/phieu-trac-nghiem.php
@@ -17,10 +17,16 @@ Template Name: Phieu Trac Nghiem
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/tracnghiem.css">
+	<link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.4.0/css/bootstrap4-toggle.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.4.0/js/bootstrap4-toggle.min.js"></script>
 </head>
 
 <body>
     <script src="https://unpkg.com/jspdf@latest/dist/jspdf.min.js"></script>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="/tnmaker">Trang chủ</a>
+		
+	</nav>
     <div id="page" class="site">
         <div id="content" class="site-content">
             <section id="primary" class="content-area">
@@ -28,12 +34,12 @@ Template Name: Phieu Trac Nghiem
 
                     <section class="make-form-tn">
                         <header class="page-header">
-                            <h1 class="page-title">JS Pdf</h1>
+                            <h2 style="text-align : center">Phiếu trắc nghiệm</h2>
                         </header><!-- .page-header -->
                         <div class="row" style="padding : 20px;width : 100%;">
                             <div style="width : 30% ;text-align : center;">
                                 <div style="text-align : left; padding : 20px;">
-                                    <form onsubmit="onSubmitForm()">
+                                    <form onsubmit="return onSubmitForm(event)">
                                         <div class="form-group">
                                             <label>Chọn mẫu đề trắc nghiệm</label>
                                             <select class="form-control" id="type_form" onchange="onChangeTypeForm()">
@@ -45,39 +51,41 @@ Template Name: Phieu Trac Nghiem
                                         </div>
                                         <div class="form-group">
                                             <label>Số lượng câu trắc nghiệm</label>
-                                            <input type="number" min="1" max="120" class="form-control" value="20" placeholder="Điền số câu hỏi" id="number_question" onchange="change_number_questtion()">
+                                            <input type="number" required min="1" max="20" class="form-control" value="20" placeholder="Điền số câu hỏi" id="number_question" onchange="change_number_questtion()">
                                         </div>
                                         <div class="form-group">
                                             <label for="customRange1">Độ đậm phiếu tô (<span id="input_preview_alpha" style="color: rgb(0, 0, 0,0.5);">50%</span>)</label>
                                             <div class="row">
-                                                <div class="col-md-1">1</div>
+                                                <div class="col-md-1" style="font-size : 18px">1</div>
                                                 <div class="col-md-10">
                                                     <input id="input_alpha" type="range" class="custom-range" min='1' value="50" max="100" onchange="change_alpha()">
                                                 </div>
-                                                <div class="col-md-1">100</div>
+                                                <div class="col-md-1" style="font-size : 18px">100</div>
                                             </div>
                                         </div>
-                                        <div class="form-group">
-                                            <label>Chỉnh sửa chi tiết</label>
+                                        <div class="form-group" id="detail_input">
+                                            <label style="display : block;">Chỉnh sửa chi tiết</label>
+											<label style="font-size : 0.7em;">Hiển thị</label>
+											<input type="checkbox" checked data-toggle="toggle" data-size="xs" data-on="Bật" data-off="Tắt" id="input_show">
                                             <div class="row" style="font-size : 0.7em;">
                                                 <div class="col-md-4 col-xs-12">
                                                     <label>Kích thước</label>
-                                                    <input type="number" value="10" id="input_size" min="5" max="20" class="form-control" onchange="change_size()">
+                                                    <input type="number" style="font-size : 18px" required value="10" id="input_size" min="5" max="20" class="form-control" onchange="change_size()">
                                                 </div>
                                                 <div class="col-md-4 col-xs-12">
                                                     <label>Kiểu</label>
                                                     <div style="width : 100%;" class="btn-group" role="group" aria-label="Basic example">
-                                                        <button type="button" id="input_bold" onclick="change_bold()" class="btn btn-outline-primary" style="font-weight : bold;">Đậm</button>
-                                                        <button type="button" id="input_italic" onclick="change_italic()" class="btn btn-outline-secondary" style="font-style: italic;">Nghiêng</button>
+                                                        <button type="button" id="input_bold" onclick="change_bold()" class="btn btn-outline-primary" style="font-weight : bold;font-size : 18px;">Đậm</button>
+                                                        <button type="button" id="input_italic" onclick="change_italic()" class="btn btn-outline-secondary" style="font-style: italic;font-size : 18px;">Nghiêng</button>
                                                     </div>
                                                 </div>
                                             </div>
                                             <label style="font-size : 0.7em;">Nội dung</label>
-                                            <textarea disabled class="form-control" id="input_content" style="min-height: 200px;"></textarea>
+											
+                                            <textarea disabled class="form-control" id="input_content" style="min-height: 200px; background-color : transparent; color : black;"></textarea>
                                         </div>
                                         <div class="row">
-                                            <button type="button" onclick="preview()" class="btn btn-outline-primary col-md-6 col-xs-12"><i class="fas fa-clipboard-check"></i>Xem trước</button>
-                                            <button type="button" onclick="download_file()" class="btn btn-success col-md-6 col-xs-12"><i class="fas fa-download"></i>Tải phiếu chấm</button>
+											<button type="submit" style="font-size : 18px" class="btn btn-success col-md-12 col-xs-12"><i class="fas fa-download"></i>Tải phiếu chấm</button>
                                         </div>
                                     </form>
                                 </div>
@@ -116,8 +124,8 @@ Template Name: Phieu Trac Nghiem
                                                 <textarea onClick="edit(6)" row="6" class="text-area-border" id='input6' style="margin-top : 0mm;margin-left : 40mm;font-size: 10px;max-width:80mm;min-height: 100px"></textarea>
                                             </div>
                                             <div class="col-md-5">
-                                                <textarea onClick="edit(7)" row="1" id='input7' class="text-area-noborder" style="margin-top : 0mm;margin-left:20mm;font-size:12px;font-weight: bold; width : 30mm;"></textarea>
-                                                <textarea onClick="edit(8)" row="1" id='input8' class="text-area-border" style="margin-top : 0mm;margin-left:15mm;font-size:12px;font-weight: bold;width : 30mm;"></textarea>
+                                                <textarea onClick="edit(7)" row="1" id='input7' class="text-area-noborder" style="margin-top : 0mm;margin-left:20mm;font-size:12px;font-weight: bold; width : 30mm;height : 6mm;"></textarea>
+                                                <textarea onClick="edit(8)" row="2" id='input8' class="text-area-border" disabled style="margin-top : 0mm;margin-left:15mm;font-size:12px;font-weight: bold;width : 25mm; height : 18mm;"></textarea>
                                             </div>
                                         </div>
                                     </div>
@@ -139,6 +147,7 @@ Template Name: Phieu Trac Nghiem
                                     document.getElementById("input6").value = "Lưu ý:\n-Ghi đầy đủ các mục, giữ phiếu phẳng\n-Bôi đen đáp án tương ứng với số câu trong đề\n- Bài kiểm tra được chấm bằng máy, học sinh tô\n đậm, vừa khít với ô tròn giới hạn. TUYỆT ĐỐI\n không sửa chữa đáp án."
                                     document.getElementById("input7").value = " ĐIỂM SỐ"
                                     document.getElementById("input8").value = ""
+									document.getElementById("detail_input").style.display = 'none'
 
                                     var style = []
                                     for (let i = 0; i < 9; i++) {
@@ -148,6 +157,7 @@ Template Name: Phieu Trac Nghiem
                                             size: 12,
                                             bold: true,
                                             italic: false,
+											show : true,
                                             x: 10,
                                             y: 10,
                                             w: 10,
@@ -166,12 +176,13 @@ Template Name: Phieu Trac Nghiem
 
 
                                 function edit(type) {
-                                    console.log(type)
                                     current = type
+									document.getElementById("detail_input").style.display = ''
                                     document.getElementById("input_content").value = document.getElementById("input" + type).value
                                     config.style[type].content = document.getElementById("input" + type).value
                                     document.getElementById("input_content").style.fontSize = config.style[type].size + "px"
                                     document.getElementById("input_size").value = config.style[type].size
+									$("#input_show").prop('checked', config.style[type].show).change()
                                     if (config.style[type].bold) {
                                         document.getElementById("input_bold").classList.add("active");
                                         document.getElementById("input_content").style.fontWeight = 'bold';
@@ -191,9 +202,7 @@ Template Name: Phieu Trac Nghiem
 
                                 function change_alpha() {
                                     let tmp = document.getElementById("input_alpha").value
-                                    console.log(tmp)
                                     config.alpha = Math.ceil(255 * tmp / 100)
-                                    console.log(config.alpha)
                                     document.getElementById("input_preview_alpha").style = "color: rgb(0, 0, 0," + tmp / 100 + ");"
                                     document.getElementById("input_preview_alpha").textContent = tmp + "%"
                                 }
@@ -207,6 +216,7 @@ Template Name: Phieu Trac Nghiem
                                         number_ques.setAttribute("max", 20)
                                         config.number_of_question = 20
                                         config.form = 20
+										document.getElementById("input6").style.display =''
                                         document.getElementById("preview_html").style = "background-size:contain  ;background-image: url('<?php bloginfo('template_directory'); ?>/img/form20_vi_6sbd_nolabel.jpg')"
                                     }
 
@@ -215,6 +225,7 @@ Template Name: Phieu Trac Nghiem
                                         number_ques.setAttribute("max", 40)
                                         config.number_of_question = 40
                                         config.form = 40
+										document.getElementById("input6").style.display =''
                                         document.getElementById("preview_html").style = "background-size:contain  ;background-image: url('<?php bloginfo('template_directory'); ?>/img/form40_vi_6sbd_nolabel.jpg')"
                                     }
 
@@ -223,6 +234,7 @@ Template Name: Phieu Trac Nghiem
                                         number_ques.setAttribute("max", 60)
                                         config.number_of_question = 60
                                         config.form = 60
+										document.getElementById("input6").style.display =''
                                         document.getElementById("preview_html").style = "background-size:contain  ;background-image: url('<?php bloginfo('template_directory'); ?>/img/form60_vi_6sbd_nolabel.jpg')"
                                     }
 
@@ -231,14 +243,8 @@ Template Name: Phieu Trac Nghiem
                                         number_ques.setAttribute("max", 100)
                                         config.number_of_question = 100
                                         config.form = 100
+										document.getElementById("input6").style.display ='none'
                                         document.getElementById("preview_html").style = "background-size:contain  ;background-image: url('<?php bloginfo('template_directory'); ?>/img/form100_vi_6sbd_nolabel.jpg')"
-                                    }
-
-                                    if (tmp == 120) {
-                                        number_ques.setAttribute("min", 101)
-                                        number_ques.setAttribute("max", 120)
-                                        config.number_of_question = 120
-                                        config.form = 120
                                     }
                                 }
 
@@ -267,6 +273,11 @@ Template Name: Phieu Trac Nghiem
                                         config.style[current].italic = false
                                     }
                                 }
+								
+								$("#input_show").change(function(){
+									let tmp = !config.style[current].show
+									config.style[current].show = tmp
+								})
 
                                 function change_size() {
                                     document.getElementById("input" + current).style.fontSize = document.getElementById("input_size").value + "px"
@@ -277,16 +288,11 @@ Template Name: Phieu Trac Nghiem
                                 function change_number_questtion() {
                                     config.number_of_question = document.getElementById("number_question").value
                                 }
-
-                                function preview() {
-                                    console.log(config)
-                                }
-
-                                function download_file() {
-
-                                }
-
-                                function onSubmitForm() {}
+								
+                                function onSubmitForm(e) {
+									e.preventDefault()
+									console.log(config)
+								}
                             </script>
                         </div><!-- .page-content -->
                     </section><!-- .no-results -->
